888a60089d2cae12dce817bd3b564f682742685a
ai-122 <hitsu.a122@gmail.com>
2026-06-18 22:12:31 +0900

    #20 取得した時刻を日本時間に変換するように修正

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,16 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_issue_manager/core/theme/app_theme.dart';
 import 'package:github_issue_manager/view/pages/root/root_page.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // .envファイルの読み込み
   await dotenv.load();
+
+  // タイムゾーンの初期化
+  tz.initializeTimeZones();
 
   runApp(
     const ProviderScope(
